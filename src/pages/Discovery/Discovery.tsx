d3c4f295f775e4cd03fb8f83e47708c1323610bf
4uphwang <4uphwang@gmail.com>
2025-09-06 15:33:23 +0900

    feat: setup react-router-dom with Suspense and basic routes

---
+++ src/pages/Discovery/Discovery.tsx
@@ -0,0 +1,21 @@
+
+const DiscoveryPage = () => {
+    return (
+        <div className="min-h-screen w-screen bg-white text-black">
+            {/* 배너 영역 */}
+            배너
+
+            {/* 즐겨찾기 영역 */}
+            <div className="p-4">
+                즐겨찾기
+            </div>
+
+            {/* 서비스 리스트 영역 */}
+            <div className="p-4">
+                목록
+            </div>
+        </div>
+    )
+}
+
+export default DiscoveryPage;
